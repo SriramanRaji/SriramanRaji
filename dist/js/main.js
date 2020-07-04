a0df24f998ba29b833199d53fc0c4e93525494e3
Sriraman Raji <sriramselvam08@gmail.com>
2020-07-04 16:39:35 +0530

    Refined UI

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,55 +1,53 @@
-// DOM Items
-const menu          = document.querySelector('.menu');
-const menuBtn       = document.querySelector('.menu-btn');
-const menuBranding  = document.querySelector('.menu-branding');
-const menuNav       = document.querySelector('.menu-nav');
-const navItems      = document.querySelectorAll('.nav-item');
-const desktopMailLink   = document.querySelector('.link-mail-desktop');
-const mobileMailLink   = document.querySelector('.link-mail-mobile');
-const desktopWaLink     = document.querySelector('.link-wa-desktop');
-const mobileWaLink      = document.querySelector('.link-wa-mobile');
+// DOM Elements
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+const menuBranding = document.querySelector(".menu-branding");
+const menuNav = document.querySelector(".menu-nav");
+const navItems = document.querySelectorAll(".nav-item");
+const desktopMailLink = document.querySelector(".link-mail-desktop");
+const mobileMailLink = document.querySelector(".link-mail-mobile");
+const desktopWaLink = document.querySelector(".link-wa-desktop");
+const mobileWaLink = document.querySelector(".link-wa-mobile");
 
-// Initial State of Menu
-let showMenu        = false;
+// Initial state of Menu
+let showMenu = false;
 
-menuBtn.addEventListener('click', toggleMenu);
+// Listening for Menu Click
+menuBtn.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-    
-    if(!showMenu) {
+  if (!showMenu) {
+    menuBtn.classList.add("close");
+    menu.classList.add("show");
+    menuNav.classList.add("show");
+    menuBranding.classList.add("show");
 
-        menuBtn.classList.add('close');
-        menu.classList.add('show');
-        menuNav.classList.add('show');
-        menuBranding.classList.add('show');
+    // Looping through all nav-links
+    navItems.forEach((element) => element.classList.add("show"));
 
-        navItems.forEach(item => item.classList.add('show'));
+    // Menu state
+    showMenu = true;
+  } else {
+    menuBtn.classList.remove("close");
+    menu.classList.remove("show");
+    menuBranding.classList.remove("show");
+    menuNav.classList.remove("show");
 
-        // set menu state
-        showMenu    = true;
+    // Looping through all nav-links
+    navItems.forEach((element) => element.classList.remove("show"));
 
-    } else {
-
-        menuBtn.classList.remove('close');
-        menu.classList.remove('show');
-        menuNav.classList.remove('show');
-        menuBranding.classList.remove('show');
-
-        navItems.forEach(item => item.classList.remove('show'));
-
-        // set menu state
-        showMenu    = false;
-
-    }
-
+    // Menu state
+    showMenu = false;
+  }
 }
 
+// Checking Mobile or Desktop
 var isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
 if (isMobile) {
-    desktopMailLink.style.display = "none";
-    desktopWaLink.style.display = "none";
+  desktopMailLink.style.display = "none";
+  desktopWaLink.style.display = "none";
 } else {
-    mobileMailLink.style.display = "none";
-    mobileWaLink.style.display = "none";    
+  mobileMailLink.style.display = "none";
+  mobileWaLink.style.display = "none";
 }
